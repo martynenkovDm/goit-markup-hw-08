52408dcc9707f9d31cb0b82e312b7b6970c61500
martynenkovDm <126678952+martynenkovDm@users.noreply.github.com>
2023-04-24 21:52:40 +0300

    add commit

---
+++ src/js/03-feedback.js
@@ -0,0 +1,32 @@
+import throttle from 'lodash.throttle';
+
+const LOCAL_KEY = 'feedback-form-state';
+let formData = JSON.parse(localStorage.getItem(LOCAL_KEY)) || {};
+
+form = document.querySelector('.feedback-form');
+
+form.addEventListener('input', throttle(storageFormData, 500));
+form.addEventListener('submit', onFormSubmit);
+
+reloadPage();
+
+function storageFormData(e) {
+  formData[e.target.name] = e.target.value.trim();
+  localStorage.setItem(LOCAL_KEY, JSON.stringify(formData));
+}
+
+function onFormSubmit(e) {
+  e.preventDefault();
+  console.log(formData);
+  e.currentTarget.reset();
+  localStorage.removeItem(LOCAL_KEY);
+  formData = {};
+}
+
+function reloadPage() {
+  if (formData) {
+    let { email, message } = form.elements;
+    email.value = formData.email || '';
+    message.value = formData.message || '';
+  }
+}
